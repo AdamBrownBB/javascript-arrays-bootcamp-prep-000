40762d46f0661cc8ed63af5b0ea03a85065f9a0f
Adam Brown <adam.brown.betty@gmail.com>
2019-06-26 21:53:17 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -41,9 +41,9 @@ function destructivelyRemoveElementFromBeginningOfArray (array) {
 }
 
 function removeElementFromBeginningOfArray (array) {
-   array = array.slice(0)
+   return array.slice(0);
   
-  return array;
+ 
 }
 
 
